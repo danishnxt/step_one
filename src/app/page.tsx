df927fc3eea6fa12f3c16c2d7a57edb9ba3b7135
danish farid <danishnxt@gmail.com>
2023-09-09 22:17:37 -0400

    better page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex h-screen justify-center items-center">
       <div>
-        Sorry sarim this is 15 days too late lool
+        <div className="text-center border-red text-xl">
+          Sorry sarim this is 15 days too late lool
+        </div>
+        <img src="https://as2.ftcdn.net/v2/jpg/01/66/10/03/1000_F_166100342_KbTGIRrnrlwGDZSXSMpH3zfn2dxyTKae.jpg" alt="Monkey Image" />
+        <div className="text-center text-red">
+          Please accept my apologies :(
+        </div>
       </div>
     </main>
   )
